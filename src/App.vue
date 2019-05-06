c111345efe4d6cb0c84ddd254dd328a28b8f2281
158109640 <158109640@qq.com>
2019-05-06 10:49:51 +0800

    浏览器兼容

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 <style>
 #app {
   max-width: 1440px;
+	margin: 0 auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
